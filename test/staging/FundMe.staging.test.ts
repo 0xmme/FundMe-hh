ebb6672f2ea9651dba50cf2799d902c24b7adc95
mme <9083787+mme022@users.noreply.github.com>
2022-06-26 17:36:46 +0200

    added the staging test for rinkeby network

---
+++ test/staging/FundMe.staging.test.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable node/no-missing-import */
+import { assert } from "chai";
+import { BigNumber } from "ethers";
+import { getNamedAccounts, ethers, network } from "hardhat";
+import { Address } from "hardhat-deploy/types";
+import { devChains } from "../../helper-hardhat-config";
+import { FundMe } from "../../typechain";
+
+devChains.includes(network.name)
+  ? describe.skip
+  : describe("FundMe", () => {
+      let deployer: Address;
+      let fundMe: FundMe;
+      const sendValue: BigNumber = ethers.utils.parseEther("0.001");
+      beforeEach(async () => {
+        deployer = (await getNamedAccounts()).deployer;
+        fundMe = await ethers.getContract("FundMe", deployer);
+      });
+      it("allows people to fund the contract", async () => {
+        await fundMe.fund({ value: sendValue });
+        await fundMe.withdraw({ gasLimit: 100000 });
+        const endBal: BigNumber = await fundMe.provider.getBalance(
+          fundMe.address
+        );
+        assert.equal(endBal.toString(), "0");
+      });
+    });
